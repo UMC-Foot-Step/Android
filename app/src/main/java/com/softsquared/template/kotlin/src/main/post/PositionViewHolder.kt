98c6d4563b43fcb4ddde7a0224ef4645a5567a40
kim <kim@gmail.com>
2023-01-17 03:06:48 +0900

    fixing .gitignore+ver1.1

---
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/post/PositionViewHolder.kt
@@ -0,0 +1,15 @@
+package com.softsquared.template.kotlin.src.main.post
+
+import androidx.recyclerview.widget.RecyclerView
+import com.softsquared.template.kotlin.databinding.SearchPositionItemListBinding
+
+class PositionViewHolder(private val binding: SearchPositionItemListBinding) : RecyclerView.ViewHolder(binding.root) {
+    private val context = binding.root.context
+
+    fun bind(item: PositionData){
+        binding.searchPosTvItemTitle.text = item.pos_title
+        binding.searchPosTvItemAddress.text = item.pos_address
+        binding.searchPosTvDistance.text = item.pos_distance.toString()
+    }
+}
+
--- app/src/main/java/com/softsquared/template/kotlin/src/main/post/PostActivity.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/post/PostActivity.kt
@@ -1,13 +1,132 @@
 package com.softsquared.template.kotlin.src.main.post
 
-
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.ImageButton
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.softsquared.template.kotlin.R
 import com.softsquared.template.kotlin.config.BaseActivity
 import com.softsquared.template.kotlin.databinding.ActivityMainPostBinding
+import java.time.LocalDate
 
 class PostActivity : BaseActivity<ActivityMainPostBinding>(ActivityMainPostBinding::inflate) {
+    // datepicker
+    private val today = LocalDate.now()
+    private var tvYear = today.year
+    private var tvMonth = today.monthValue
+    private var tvDay = today.dayOfMonth
+
+    // 갤러리 앱 열기
+    private val OPEN_GALLERY = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 뒤로가기 버튼 누르면 뒤로가기..!
+        binding.postIbBack.setOnClickListener {
+            finish()
+        }
+
+        binding.postIbCalendar.setOnClickListener {
+            val bottomSheet = layoutInflater.inflate(R.layout.fragment_post_calendar, null)
+            val bottomSheetDialog = BottomSheetDialog(this)
+            val btnClose = bottomSheet.findViewById<ImageButton>(R.id.calBtnQuit)
+            val btnDateCheck = bottomSheet.findViewById<Button>(R.id.calBtnCheck)
+            val calView = bottomSheet.findViewById<CalendarView>(R.id.calDatePicker)
+
+            calView.maxDate = System.currentTimeMillis()
+
+            // calendar bottomsheetdialog 설정
+            bottomSheetDialog.setContentView(bottomSheet)
+            bottomSheetDialog.show()
+
+            btnClose.setOnClickListener {
+                bottomSheetDialog.dismiss()
+            }
+
+            calView.setOnDateChangeListener { calView, year, month, dayOfMonth ->
+                tvYear = year
+                tvMonth = month + 1
+                tvDay = dayOfMonth
+            }
+
+            btnDateCheck.setOnClickListener{
+                dataSet(tvYear, tvMonth, tvDay)
+                bottomSheetDialog.dismiss()
+            }
+        }
+
+        // datePicker 설정
+        dataSet(tvYear, tvMonth, tvDay)
+
+        // 위치 검색 activity 전환
+        binding.postBtnLoc.setOnClickListener{
+            val intent = Intent(this, PostSearchPositionActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // 갤러리 열기
+        binding.postIbGallery.setOnClickListener {
+            openGallery()
+        }
+        
+        // 작성하기 버튼 눌렀을 때 값 저장
+        binding.postBtnPost.setOnClickListener {
+            finish()
+        }
+
+        if(intent.hasExtra("positionTitle")){
+            binding.postBtnLoc.text = intent.getStringExtra("positionTitle")
+        }
     }
+    override fun onRestart() {
+        super.onRestart()
+        // 장소 제목 불러오기
+        if(intent.hasExtra("positionTitle")){
+            binding.postBtnLoc.text = intent.getStringExtra("positionTitle")
+        }
+    }
+    private fun dataSet(year: Int, month: Int, day: Int){
+        tvYear = year
+        tvMonth = month
+        tvDay = day
+        var date = "$tvYear / $tvMonth / $tvDay"
+        binding.postTvDate.text = date
+    }
+
+    private fun openGallery(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("image/*")
+        @Suppress("DEPRECATION")
+        startActivityForResult(intent, OPEN_GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == OPEN_GALLERY){
+                var currentImageUrl : Uri? = data?.data
+
+                try{
+                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, currentImageUrl)
+                    binding.postIbGallery.setImageBitmap(bitmap)
+                } catch(e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        else{
+            Log.d("ActivityResult", "something wrong")
+        }
+    }
+
 }
