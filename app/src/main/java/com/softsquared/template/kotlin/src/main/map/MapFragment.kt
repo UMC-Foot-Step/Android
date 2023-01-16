eec4f7b18bc6d29c2c527505763a83fae26dd8f8
hhj <sbs98010@naver.com>
2023-01-16 18:15:40 +0900

    [FEAT] 더미데이터 써서 1.1 기능 구현 + 💄[UI] activity_search.xml파일 추가

--- app/src/main/java/com/softsquared/template/kotlin/src/main/map/MapFragment.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/map/MapFragment.kt
@@ -1,30 +1,60 @@
 package com.softsquared.template.kotlin.src.main.map
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.marginRight
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 import com.softsquared.template.kotlin.R
 import com.softsquared.template.kotlin.config.BaseFragment
 import com.softsquared.template.kotlin.databinding.FragmentMapBinding
 import com.softsquared.template.kotlin.src.main.MainActivity
+import com.softsquared.template.kotlin.src.main.map.search.SearchActivity
 import kotlin.concurrent.thread
 
 class MapFragment :
     BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R.layout.fragment_map),
     OnMapReadyCallback {
+    private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
+    private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
+    private lateinit var naverMap: NaverMap
+    lateinit var layoutParams:ConstraintLayout.LayoutParams
+
+    private val markerList:ArrayList<Marker> = ArrayList()
+
+   // private val latlngList=Array<Array<Double>>(3){Array<Double>(3){0.0} }
+    //private val latlngList=Array(4){DoubleArray(2){0.0} }
+    private val latlngList=arrayOf(arrayOf(37.56661,126.97839), arrayOf(37.56590, 126.98223),
+       arrayOf(37.56905, 126.97767),arrayOf(37.33986, 126.74655))
+
     private lateinit var mContext: Context
     private lateinit var v1:View
-    val btnSearchInt:Int=1
-    val btnPositionInt:Int=2
+
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -42,9 +72,16 @@ class MapFragment :
        지금은 viewModel써서 하라는데 mvvm 모름..!
         */
         v1 = layoutInflater.inflate(R.layout.feetstep_info, binding.placeInfo, true)
+        layoutParams = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.MATCH_PARENT,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
+
 
         binding.mapView?.onCreate(savedInstanceState)
         binding.mapView?.getMapAsync(this)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
 
         initButtonView()
     }
@@ -54,13 +91,16 @@ class MapFragment :
             if (!binding.btnMenu.isSelected) {
                 binding.btnMenu.background =
                     ContextCompat.getDrawable(mContext, R.drawable.menu_btn_selected_background)
+
                 binding.subBtnLayout.visibility = View.VISIBLE
+
                 binding.btnMenu.isSelected = true
             } else {
                 binding.btnMenu.background =
                     ContextCompat.getDrawable(mContext, R.drawable.menu_btn_background)
-                binding.subBtnLayout.visibility = View.GONE
                 binding.btnMenu.isSelected = false
+                binding.subBtnLayout.visibility = View.GONE
+                binding.btnTime.isSelected = false
             }
         }
 
@@ -69,7 +109,7 @@ class MapFragment :
                 binding.btnTime.background =
                     ContextCompat.getDrawable(mContext, R.drawable.time_btn_selected_background)
                 binding.btnTime.isSelected = true
-            } else if (binding.btnTime.isSelected){
+            } else {
                 binding.btnTime.background =
                     ContextCompat.getDrawable(mContext, R.drawable.time_btn_background)
                 binding.btnTime.isSelected = false
@@ -92,40 +132,21 @@ class MapFragment :
 
         binding.btnSearch.setOnClickListener{
             if (!binding.btnSearch.isSelected) {
-                desplayFeetStepInfoView(btnSearchInt)
                 binding.btnSearch.isSelected = true
+
+                val intent= Intent(requireContext(), SearchActivity::class.java)
+                startActivity(intent)
             } else {
-                binding.placeInfo.visibility=View.GONE
                 binding.btnSearch.isSelected = false
             }
         }
 
         binding.btnPosition.setOnClickListener {
-            if (!binding.btnPosition.isSelected) {
-                desplayFeetStepInfoView(btnPositionInt)
-                binding.btnPosition.isSelected = true
-            } else {
-                binding.placeInfo.visibility=View.GONE
-                binding.btnPosition.isSelected = false
-            }
+            naverMap.locationSource = locationSource
+            naverMap.locationTrackingMode = LocationTrackingMode.Follow
         }
     }
 
-    fun desplayFeetStepInfoView(btn:Int) {
-        binding.placeInfo.visibility=View.VISIBLE
-
-        v1.findViewById<TextView>(R.id.titleTextView).text = "장소이름"
-
-        if(btn==btnPositionInt) {
-            v1.findViewById<CardView>(R.id.cardview_member).visibility = View.VISIBLE
-            v1.findViewById<TextView>(R.id.priceTextView).text = "내 발자취 1개"
-        }
-        else {
-            v1.findViewById<CardView>(R.id.cardview_member).visibility = View.GONE
-            v1.findViewById<TextView>(R.id.priceTextView).text = "방문하지 않은 곳"
-        }
-
-    }
 
     override fun onStart() {
         super.onStart()
@@ -147,10 +168,6 @@ class MapFragment :
         binding.mapView.onStop()
     }
 
-    //override fun onDestroyView() {
-    //    super.onDestroyView()
-    //    binding.mapView?.onDestroy()
-    //}
 
     override fun onLowMemory() {
         super.onLowMemory()
@@ -158,6 +175,93 @@ class MapFragment :
     }
 
 
-    override fun onMapReady(p0: NaverMap) {
+    override fun onMapReady(@NonNull naverMap: NaverMap) {
+        this.naverMap = naverMap
+
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+
+        for((i,value) in latlngList.withIndex()) {
+            //var marker1 =
+            if(i<2) {
+                Marker().apply {
+                    position = LatLng(value[0], value[1])
+                    map = naverMap
+                    icon= OverlayImage.fromResource(R.drawable.footstep_orange)
+                    tag=false
+
+                    setOnClickListener {
+                        val important = tag as Boolean
+                        setImportant(true,this, important)
+
+                        //desplayFeetStepInfoView(true)
+                        //Toast.makeText(mContext, "마커 클릭됨", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                }
+            }
+
+            else{
+                Marker().apply {
+                    position = LatLng(value[0], value[1])
+                    map = naverMap
+                    icon= OverlayImage.fromResource(R.drawable.foot3)
+                    tag=false
+
+                    setOnClickListener {
+                        val important = tag as Boolean
+                        setImportant(false,this, important)
+
+                       // desplayFeetStepInfoView(false)
+                        //Toast.makeText(mContext, "마커 클릭됨", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                }
+            }
+        }
+
+        naverMap.setOnMapClickListener{  _, coord ->
+            binding.placeInfo.visibility=View.GONE
+        }
     }
+
+    fun setImportant(marker_type:Boolean,marker: Marker, important: Boolean) {
+        if(important==false){
+            desplayFeetStepInfoView(marker_type)
+            marker.tag = true
+        }
+        else{
+            binding.placeInfo.visibility=View.GONE
+            marker.tag = false
+        }
+    }
+
+    fun desplayFeetStepInfoView(marker_type:Boolean) {
+        binding.placeInfo.visibility=View.VISIBLE
+
+        v1.findViewById<TextView>(R.id.titleTextView).text = "장소이름"
+
+        if(marker_type) {
+            v1.findViewById<CardView>(R.id.cardview_member).visibility = View.VISIBLE
+            //v1.
+
+            val textData: String = "내 발자취 1개"
+
+            val spannable = SpannableStringBuilder(textData)
+            val boldSpan = StyleSpan(Typeface.BOLD)
+            spannable.setSpan(boldSpan, 6, 7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            val colorSpan = ForegroundColorSpan( ContextCompat.getColor(mContext, R.color.orange))
+            spannable.setSpan(colorSpan, 6, 7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            v1.findViewById<TextView>(R.id.priceTextView).text = spannable
+        }
+
+        else {
+            v1.findViewById<CardView>(R.id.cardview_member).visibility = View.GONE
+            v1.findViewById<TextView>(R.id.priceTextView).text = "방문하지 않은 곳"
+        }
+
+    }
+
 }
--- app/src/main/java/com/softsquared/template/kotlin/src/main/map/search/SearchActivity.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/map/search/SearchActivity.kt
@@ -1,4 +1,15 @@
 package com.softsquared.template.kotlin.src.main.map.search
 
-class SearchActivity {
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.softsquared.template.kotlin.config.BaseActivity
+import com.softsquared.template.kotlin.databinding.ActivitySearchBinding
+
+class SearchActivity : BaseActivity<ActivitySearchBinding>(ActivitySearchBinding::inflate){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
 }
