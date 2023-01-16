c90e0c3277b0a42dedc4c96d2c477042248d8dbe
hhj <sbs98010@naver.com>
2023-01-16 19:33:44 +0900

    ⚡️더미데이터 다양하게 써서 1.1 완료

--- app/src/main/java/com/softsquared/template/kotlin/src/main/map/MapFragment.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/map/MapFragment.kt
@@ -2,26 +2,19 @@ package com.softsquared.template.kotlin.src.main.map
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
-import android.util.Log
 import android.view.View
-import android.widget.LinearLayout
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toDrawable
-import androidx.core.view.marginRight
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.NaverMap
@@ -29,13 +22,10 @@ import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
-import com.naver.maps.map.util.MarkerIcons
 import com.softsquared.template.kotlin.R
 import com.softsquared.template.kotlin.config.BaseFragment
 import com.softsquared.template.kotlin.databinding.FragmentMapBinding
-import com.softsquared.template.kotlin.src.main.MainActivity
 import com.softsquared.template.kotlin.src.main.map.search.SearchActivity
-import kotlin.concurrent.thread
 
 class MapFragment :
     BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R.layout.fragment_map),
@@ -43,14 +33,14 @@ class MapFragment :
     private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
     private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
     private lateinit var naverMap: NaverMap
-    lateinit var layoutParams:ConstraintLayout.LayoutParams
 
-    private val markerList:ArrayList<Marker> = ArrayList()
-
-   // private val latlngList=Array<Array<Double>>(3){Array<Double>(3){0.0} }
+    //private val markerList:ArrayList<Marker> = ArrayList()
+    //private val latlngList=Array<Array<Double>>(3){Array<Double>(3){0.0} }
     //private val latlngList=Array(4){DoubleArray(2){0.0} }
-    private val latlngList=arrayOf(arrayOf(37.56661,126.97839), arrayOf(37.56590, 126.98223),
-       arrayOf(37.56905, 126.97767),arrayOf(37.33986, 126.74655))
+    val latlngList=arrayOf(arrayOf(37.56661,126.97839), arrayOf(37.56590, 126.98223), arrayOf(37.56905, 126.97767),
+       arrayOf(37.33986, 126.74655),arrayOf(37.55946, 126.97514),arrayOf(37.57174, 126.97644))
+    val locationList=arrayOf("서울특별시청","을지로입구역","청계광장","왕동","숭례문","광화문역")
+    val imageList=arrayOf(R.drawable.cat_dummy, R.drawable.im2_dummy, R.drawable.google_dummy)
 
     private lateinit var mContext: Context
     private lateinit var v1:View
@@ -59,7 +49,7 @@ class MapFragment :
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
-        // 2. Context를 액티비티로 형변환해서 할당
+        // 메인 액티비티 context 획득
         mContext = context
     }
 
@@ -68,19 +58,14 @@ class MapFragment :
 
         setRetainInstance(true);
         /* Activity가 onDestroy되고 재생성될 때 Fragment도 같이 재생성된다. 하지만 setRetainInstance(true)를 설정하면
-       onCreate, onDestory가 호출되지 않고 재사용된다.
-       지금은 viewModel써서 하라는데 mvvm 모름..!
+       onCreate, onDestory가 호출되지 않고 재사용된다.->현재는 mvvm패턴의 viewModel써서 하라는데 몰라서 저거 일단 둠
         */
         v1 = layoutInflater.inflate(R.layout.feetstep_info, binding.placeInfo, true)
-        layoutParams = ConstraintLayout.LayoutParams(
-            ConstraintLayout.LayoutParams.MATCH_PARENT,
-            ConstraintLayout.LayoutParams.WRAP_CONTENT
-        )
-
 
         binding.mapView?.onCreate(savedInstanceState)
         binding.mapView?.getMapAsync(this)
 
+        //현재위치 기능
         locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
 
         initButtonView()
@@ -93,11 +78,11 @@ class MapFragment :
                     ContextCompat.getDrawable(mContext, R.drawable.menu_btn_selected_background)
 
                 binding.subBtnLayout.visibility = View.VISIBLE
-
                 binding.btnMenu.isSelected = true
             } else {
                 binding.btnMenu.background =
                     ContextCompat.getDrawable(mContext, R.drawable.menu_btn_background)
+
                 binding.btnMenu.isSelected = false
                 binding.subBtnLayout.visibility = View.GONE
                 binding.btnTime.isSelected = false
@@ -131,14 +116,8 @@ class MapFragment :
         }
 
         binding.btnSearch.setOnClickListener{
-            if (!binding.btnSearch.isSelected) {
-                binding.btnSearch.isSelected = true
-
-                val intent= Intent(requireContext(), SearchActivity::class.java)
-                startActivity(intent)
-            } else {
-                binding.btnSearch.isSelected = false
-            }
+            val intent= Intent(requireContext(), SearchActivity::class.java)
+            startActivity(intent)
         }
 
         binding.btnPosition.setOnClickListener {
@@ -168,7 +147,6 @@ class MapFragment :
         binding.mapView.onStop()
     }
 
-
     override fun onLowMemory() {
         super.onLowMemory()
         binding.mapView.onLowMemory()
@@ -181,10 +159,8 @@ class MapFragment :
         naverMap.locationSource = locationSource
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
 
-
         for((i,value) in latlngList.withIndex()) {
-            //var marker1 =
-            if(i<2) {
+            if(i<4) {
                 Marker().apply {
                     position = LatLng(value[0], value[1])
                     map = naverMap
@@ -192,11 +168,9 @@ class MapFragment :
                     tag=false
 
                     setOnClickListener {
-                        val important = tag as Boolean
-                        setImportant(true,this, important)
+                        //val important = tag as Boolean
+                        setMarkerClickEvent(true,this, tag as Boolean,i)
 
-                        //desplayFeetStepInfoView(true)
-                        //Toast.makeText(mContext, "마커 클릭됨", Toast.LENGTH_SHORT).show()
                         true
                     }
                 }
@@ -210,11 +184,8 @@ class MapFragment :
                     tag=false
 
                     setOnClickListener {
-                        val important = tag as Boolean
-                        setImportant(false,this, important)
+                        setMarkerClickEvent(false,this, tag as Boolean,i)
 
-                       // desplayFeetStepInfoView(false)
-                        //Toast.makeText(mContext, "마커 클릭됨", Toast.LENGTH_SHORT).show()
                         true
                     }
                 }
@@ -225,10 +196,17 @@ class MapFragment :
             binding.placeInfo.visibility=View.GONE
         }
     }
-
-    fun setImportant(marker_type:Boolean,marker: Marker, important: Boolean) {
-        if(important==false){
-            desplayFeetStepInfoView(marker_type)
+/*더미데이터 상에서는 marker_type이 굳이 필요하지않지만
+ 나중에 진짜 데이터 받았을땐 marker_num이 무쓸모&marker_type은 쓸모
+ */
+    fun setMarkerClickEvent(
+        marker_type:Boolean,
+        marker: Marker,
+        tag: Boolean,
+        marker_num:Int
+    ) {
+        if(tag==false){
+            desplayFeetStepInfoView(marker_type,marker_num)
             marker.tag = true
         }
         else{
@@ -237,16 +215,16 @@ class MapFragment :
         }
     }
 
-    fun desplayFeetStepInfoView(marker_type:Boolean) {
+    fun desplayFeetStepInfoView(
+        marker_type:Boolean,
+        marker_num:Int
+    ) {
         binding.placeInfo.visibility=View.VISIBLE
-
-        v1.findViewById<TextView>(R.id.titleTextView).text = "장소이름"
 
         if(marker_type) {
             v1.findViewById<CardView>(R.id.cardview_member).visibility = View.VISIBLE
-            //v1.
 
-            val textData: String = "내 발자취 1개"
+            val textData: String = "내 발자취 ${marker_num+1}개"
 
             val spannable = SpannableStringBuilder(textData)
             val boldSpan = StyleSpan(Typeface.BOLD)
@@ -254,14 +232,20 @@ class MapFragment :
             val colorSpan = ForegroundColorSpan( ContextCompat.getColor(mContext, R.color.orange))
             spannable.setSpan(colorSpan, 6, 7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
-            v1.findViewById<TextView>(R.id.priceTextView).text = spannable
+            v1.findViewById<TextView>(R.id.feetTextView).text = spannable
+            v1.findViewById<TextView>(R.id.titleTextView).text = locationList[marker_num]
+            
+            if(marker_num<3){
+                v1.findViewById<ImageView>(R.id.thumbnailImageView).setImageResource(imageList[marker_num])
+            }
+
         }
 
         else {
             v1.findViewById<CardView>(R.id.cardview_member).visibility = View.GONE
-            v1.findViewById<TextView>(R.id.priceTextView).text = "방문하지 않은 곳"
+
+            v1.findViewById<TextView>(R.id.feetTextView).text = "방문하지 않은 곳"
+            v1.findViewById<TextView>(R.id.titleTextView).text = locationList[marker_num]
         }
-
     }
-
 }
