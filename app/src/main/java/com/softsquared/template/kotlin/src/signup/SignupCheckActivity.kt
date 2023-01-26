a00d8557500ca4dae27604dad59100a8851cad80
DAHLIACHOI <cyj000424@naver.com>
2023-01-27 01:17:14 +0900

    회원가입 뷰 구현

--- app/src/main/java/com/softsquared/template/kotlin/src/signup/SignupCheckActivity.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/signup/SignupCheckActivity.kt
@@ -1,4 +1,15 @@
 package com.softsquared.template.kotlin.src.signup
 
-class SignupCheckActivity {
+import android.os.Bundle
+import com.softsquared.template.kotlin.R
+import com.softsquared.template.kotlin.config.BaseActivity
+import com.softsquared.template.kotlin.databinding.ActivityMainPostBinding
+
+class SignupCheckActivity : BaseActivity<ActivityMainPostBinding>(ActivityMainPostBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_check)
+
+    }
 }
