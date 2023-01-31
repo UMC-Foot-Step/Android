564d857ebfb1799ae03f2b75e289ddf5e5ff3cce
kim <kim@gmail.com>
2023-01-31 23:39:22 +0900

    error ver2

--- app/src/main/java/com/softsquared/template/kotlin/src/main/postupdate/PostUpdateService.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/postupdate/PostUpdateService.kt
@@ -9,6 +9,7 @@ import retrofit2.Response
 class PostUpdateService(val postUpdateActivityInterface: PostUpdateActivityInterface) {
     // api 연결 테스트
     fun getPostUpdateInfo(postingId: Int){
+        ApplicationClass.sSharedPreferences.edit().putString(ApplicationClass.X_ACCESS_TOKEN, "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImZvb3RzdGVwQG5hdmVyLmNvbSIsImlhdCI6MTY3NDkxNDc2NiwiZXhwIjoxNjc1MjE3MTY2fQ.KxwX1Q0o-omU1rRIiUJBd9gLPbTRVciP_9g_sklW1Bk").apply()
         val postUpdateRetrofitInterface = ApplicationClass.sRetrofit.create(PostUpdateRetrofitInterface::class.java)
         postUpdateRetrofitInterface.getPostUpdateInfo(postingId).enqueue(object :
             Callback<PostUpdateResponse> {
